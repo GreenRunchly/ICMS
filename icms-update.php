c60b114b2dbb41de8baefc55803692e497b10ed2
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2023-08-29 00:26:57 +0700

    Apply Update

--- icms-update.php
+++ icms-update.php
@@ -9,7 +9,7 @@
 		fclose( $fileupdater );
 
 		$updatestatus = json_decode(file_get_contents('https://raw.githubusercontent.com/GreenRunchly/icms/main/server/latest.json'), true);
-		$updatestatus[0][1] = '1.7';
+		$updatestatus[0][1] = '1.8';
 
 		if ( ! empty( $updatestatus[0][0] ) ){
 
